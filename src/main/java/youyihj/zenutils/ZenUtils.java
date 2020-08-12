572e17d0cc2102a23572d7202aee072279660f0e
youyihj <youyi580@qq.com>
2020-08-12 08:33:06 +0800

    1.0.1

--- src/main/java/youyihj/zenutils/ZenUtils.java
+++ src/main/java/youyihj/zenutils/ZenUtils.java
@@ -6,6 +6,6 @@ import net.minecraftforge.fml.common.Mod;
 public class ZenUtils {
     public static final String MODID = "zenutils";
     public static final String NAME = "ZenUtils";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String DEPENDENCIES = "required-after:crafttweaker;after:contenttweaker";
 }
