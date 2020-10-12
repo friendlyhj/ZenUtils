c30a935fcfbae0b31da008c55f805da1a32beef4
youyihj <youyi580@qq.com>
2020-10-12 21:22:28 +0800

    version dump

--- src/main/java/youyihj/zenutils/ZenUtils.java
+++ src/main/java/youyihj/zenutils/ZenUtils.java
@@ -11,7 +11,7 @@ import youyihj.zenutils.util.ZenUtilsGlobal;
 public class ZenUtils {
     public static final String MODID = "zenutils";
     public static final String NAME = "ZenUtils";
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
     public static final String DEPENDENCIES = "required-after:crafttweaker;after:contenttweaker;required-after:redstoneflux";
 
     @Mod.EventHandler
