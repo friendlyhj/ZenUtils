57caa6242123ac5e00b0ed51b988ec9396480c92
youyihj <youyi580@qq.com>
2021-01-18 22:53:25 +0800

    dependencies info

--- src/main/java/youyihj/zenutils/ZenUtils.java
+++ src/main/java/youyihj/zenutils/ZenUtils.java
@@ -25,7 +25,7 @@ public class ZenUtils {
     public static final String MODID = "zenutils";
     public static final String NAME = "ZenUtils";
     public static final String VERSION = "1.6.2";
-    public static final String DEPENDENCIES = "required-after:crafttweaker;after:contenttweaker;required-after:redstoneflux";
+    public static final String DEPENDENCIES = "required-after:crafttweaker;after:contenttweaker;required-after:redstoneflux;after:ftbquests";
 
     @Mod.EventHandler
     public static void onConstruct(FMLConstructionEvent event) {
