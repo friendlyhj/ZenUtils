03f54ec25b9ee8536f64d03b30a4ec430064b7aa
youyihj <youyi580@qq.com>
2021-04-28 15:29:57 +0800

    1.7.0

--- src/main/java/youyihj/zenutils/ZenUtils.java
+++ src/main/java/youyihj/zenutils/ZenUtils.java
@@ -35,7 +35,7 @@ import java.lang.reflect.Field;
 public class ZenUtils {
     public static final String MODID = "zenutils";
     public static final String NAME = "ZenUtils";
-    public static final String VERSION = "1.6.8";
+    public static final String VERSION = "1.7.0";
     public static final String DEPENDENCIES = "required-after:crafttweaker;after:contenttweaker;required-after:redstoneflux;after:ftbquests";
 
     public static Logger forgeLogger;
