4c0a67ae343237b3a5e1d2b0a789977f882d1632
youyihj <youyi580@qq.com>
2021-05-01 08:59:04 +0800

    20s clear cache

--- src/main/java/youyihj/zenutils/util/delay/DelayHandler.java
+++ src/main/java/youyihj/zenutils/util/delay/DelayHandler.java
@@ -12,8 +12,10 @@ public class DelayHandler {
         if (event.phase == TickEvent.Phase.END) return;
         long time = CraftTweaker.server.getEntityWorld().getWorldTime();
         DelayManager.DELAY_RUNNABLES.headMap(time, true).values().forEach(DelayRunnableList::run);
-        synchronized (DelayHandler.class) {
-            DelayManager.DELAY_RUNNABLES.keySet().removeIf(it -> it <= time);
+        if (time % 400 == 0) {
+            synchronized (DelayHandler.class) {
+                DelayManager.DELAY_RUNNABLES.keySet().removeIf(it -> it <= time);
+            }
         }
     }
 }
