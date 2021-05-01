34cf22776b54c6ab7e74bed160ab764e2ffedfb9
youyihj <youyi580@qq.com>
2021-05-01 08:26:53 +0800

    refactor delay manager

--- src/main/java/youyihj/zenutils/util/delay/DelayHandler.java
+++ src/main/java/youyihj/zenutils/util/delay/DelayHandler.java
@@ -4,14 +4,16 @@ import crafttweaker.mc1120.CraftTweaker;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import org.apache.commons.lang3.tuple.Pair;
 
 @Mod.EventBusSubscriber
 public class DelayHandler {
     @SubscribeEvent
     public static void onServerTick(TickEvent.ServerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END) return;
         long time = CraftTweaker.server.getEntityWorld().getWorldTime();
-        DelayManager.DELAY_RUNNABLE_LIST.stream().filter(pair -> pair.getRight() <= time).map(Pair::getLeft).forEach(DelayRunnable::run);
-        DelayManager.DELAY_RUNNABLE_LIST.removeIf(pair -> pair.getRight() <= time);
+        DelayManager.DELAY_RUNNABLES.headMap(time, true).values().forEach(DelayRunnableList::run);
+        synchronized (DelayHandler.class) {
+            DelayManager.DELAY_RUNNABLES.keySet().removeIf(it -> it <= time);
+        }
     }
 }
--- src/main/java/youyihj/zenutils/util/delay/DelayManager.java
+++ src/main/java/youyihj/zenutils/util/delay/DelayManager.java
@@ -1,22 +1,21 @@
 package youyihj.zenutils.util.delay;
 
 import crafttweaker.annotations.ZenRegister;
-import crafttweaker.mc1120.CraftTweaker;
-import org.apache.commons.lang3.tuple.Pair;
 import stanhebben.zenscript.annotations.Optional;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 @ZenRegister
 @ZenClass("mods.zenutils.DelayManager")
 public class DelayManager {
-    static final List<Pair<DelayRunnable, Long>> DELAY_RUNNABLE_LIST = new ArrayList<>();
+    static final NavigableMap<Long, DelayRunnableList> DELAY_RUNNABLES = new TreeMap<>();
 
     @ZenMethod
     public static void addDelayWork(DelayRunnable runnable, @Optional(valueLong = 1L) long delay) {
-        DELAY_RUNNABLE_LIST.add(Pair.of(runnable, CraftTweaker.server.getEntityWorld().getWorldTime() + delay));
+        DELAY_RUNNABLES.putIfAbsent(delay, new DelayRunnableList());
+        DELAY_RUNNABLES.get(delay).add(runnable);
     }
 }
