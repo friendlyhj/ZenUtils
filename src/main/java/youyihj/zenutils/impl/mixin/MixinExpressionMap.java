c777d33ac0e20e42f35cba015f38ec2da95544c1
youyihj <youyi580@qq.com>
2023-03-15 16:10:32 +0800

    orderly map (value[key]$orderly)

---
+++ src/main/java/youyihj/zenutils/impl/mixin/MixinExpressionMap.java
@@ -0,0 +1,31 @@
+package youyihj.zenutils.impl.mixin;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import stanhebben.zenscript.expression.ExpressionMap;
+import stanhebben.zenscript.type.ZenTypeAssociative;
+import youyihj.zenutils.impl.zenscript.IOrderlyType;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+/**
+ * @author youyihj
+ */
+@Mixin(value = ExpressionMap.class, remap = false)
+public class MixinExpressionMap {
+    @Shadow
+    @Final
+    private ZenTypeAssociative type;
+
+    @ModifyConstant(
+            method = "compile",
+            constant = @Constant(classValue = HashMap.class)
+    )
+    private Class<?> modifyCreateMapType(Class<?> clazz) {
+        return ((IOrderlyType) type).isOrderly() ? LinkedHashMap.class : HashMap.class;
+    }
+}
---
+++ src/main/java/youyihj/zenutils/impl/mixin/MixinZenTypeAssociate.java
@@ -0,0 +1,24 @@
+package youyihj.zenutils.impl.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import stanhebben.zenscript.type.ZenType;
+import stanhebben.zenscript.type.ZenTypeAssociative;
+import youyihj.zenutils.impl.zenscript.IOrderlyType;
+
+/**
+ * @author youyihj
+ */
+@Mixin(value = ZenTypeAssociative.class, remap = false)
+public abstract class MixinZenTypeAssociate extends ZenType implements IOrderlyType {
+    private boolean orderly;
+
+    @Override
+    public boolean isOrderly() {
+        return orderly;
+    }
+
+    @Override
+    public void setOrderly() {
+        this.orderly = true;
+    }
+}
