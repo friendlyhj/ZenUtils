1e8e024a5084fb869ce9b61c6936674348888b69
youyihj <youyi580@qq.com>
2023-04-21 11:54:30 +0800

    cache method handle

--- src/main/java/youyihj/zenutils/impl/reload/AnnotatedActionReloadCallback.java
+++ src/main/java/youyihj/zenutils/impl/reload/AnnotatedActionReloadCallback.java
@@ -7,28 +7,37 @@ import youyihj.zenutils.api.reload.ActionReloadCallback;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author youyihj
  */
 public class AnnotatedActionReloadCallback extends ActionReloadCallback<IAction> {
     private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
+    private static final Map<Class<?>, Optional<MethodHandle>> APPLY_RELOAD_METHOD_CACHE = new HashMap<>();
+    private static final Map<Class<?>, Optional<MethodHandle>> UNDO_METHOD_CACHE = new HashMap<>();
 
-    private MethodHandle applyReloadMethod;
-    private MethodHandle undoMethod;
+    private final MethodHandle applyReloadMethod;
+    private final MethodHandle undoMethod;
 
     public AnnotatedActionReloadCallback(IAction action) {
         super(action);
-        try {
-            this.applyReloadMethod = LOOKUP.findVirtual(action.getClass(), "applyReload", MethodType.methodType(Void.TYPE));
-        } catch (Exception e) {
-            this.applyReloadMethod = null;
-        }
-        try {
-            this.undoMethod = LOOKUP.findVirtual(action.getClass(), "undo", MethodType.methodType(Void.TYPE));
-        } catch (Exception e) {
-            this.undoMethod = null;
-        }
+        this.applyReloadMethod = APPLY_RELOAD_METHOD_CACHE.computeIfAbsent(action.getClass(), it -> {
+            try {
+                return Optional.of(LOOKUP.findVirtual(action.getClass(), "applyReload", MethodType.methodType(Void.TYPE)));
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        }).orElse(null);
+        this.undoMethod = UNDO_METHOD_CACHE.computeIfAbsent(action.getClass(), it -> {
+            try {
+                return Optional.of(LOOKUP.findVirtual(action.getClass(), "undo", MethodType.methodType(Void.TYPE)));
+            } catch (Exception e) {
+                return Optional.empty();
+            }
+        }).orElse(null);
     }
 
     @Override
