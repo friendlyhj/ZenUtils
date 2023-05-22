61bbad8893f3476726d55e28e2da2df1d9aa21b9
youyihj <youyi580@qq.com>
2023-05-22 13:56:37 +0800

    collision algorithm to get proper block state. v1.13.10

--- src/main/java/youyihj/zenutils/ZenUtils.java
+++ src/main/java/youyihj/zenutils/ZenUtils.java
@@ -39,7 +39,7 @@ import java.lang.reflect.Field;
 public class ZenUtils {
     public static final String MODID = "zenutils";
     public static final String NAME = "ZenUtils";
-    public static final String VERSION = "1.13.9";
+    public static final String VERSION = "1.13.10";
     public static final String DEPENDENCIES = "required-after:crafttweaker;after:contenttweaker;required-after:redstoneflux;after:ftbquests;required-after:mixinbooter";
 
     public static Logger forgeLogger;
--- src/main/java/youyihj/zenutils/api/event/EntityItemFallEvent.java
+++ src/main/java/youyihj/zenutils/api/event/EntityItemFallEvent.java
@@ -1,11 +1,19 @@
 package youyihj.zenutils.api.event;
 
 import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.block.IBlockState;
 import crafttweaker.api.entity.IEntityItem;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenGetter;
+
+import java.util.List;
 
 /**
  * @author youyihj
@@ -29,5 +37,16 @@ public class EntityItemFallEvent {
     @ZenGetter("distance")
     public float getDistance() {
         return distance;
+    }
+
+    @ZenGetter("blockState")
+    public IBlockState getBlockState() {
+        World world = entityItem.world;
+        List<AxisAlignedBB> aabbList =  world.getCollisionBoxes(entityItem, entityItem.getEntityBoundingBox().offset(0, -0.2, 0));
+        if (!aabbList.isEmpty()) {
+            AxisAlignedBB aabb = aabbList.get(0);
+            return CraftTweakerMC.getBlockState(world.getBlockState(new BlockPos(MathHelper.floor(aabb.minX), MathHelper.floor(aabb.minY), MathHelper.floor(aabb.minZ))));
+        }
+        return CraftTweakerMC.getBlockState(Blocks.AIR.getDefaultState());
     }
 }
