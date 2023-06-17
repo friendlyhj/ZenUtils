0f3a207b69248b938b0600c92d902c86bc36c4e0
youyihj <youyi580@qq.com>
2023-06-17 10:42:03 +0800

    Fix CoT item bracket handler doesn't resolve * wildcard

---
+++ src/main/java/youyihj/zenutils/impl/mixin/MixinCoTItemBracketHandler.java
@@ -0,0 +1,20 @@
+package youyihj.zenutils.impl.mixin;
+
+import com.teamacronymcoders.contenttweaker.modules.vanilla.resources.ItemBracketHandler;
+import net.minecraftforge.oredict.OreDictionary;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * @author youyihj
+ */
+@Pseudo
+@Mixin(value = ItemBracketHandler.class, remap = false)
+public abstract class MixinCoTItemBracketHandler {
+    @Redirect(method = "resolve", at = @At(value = "INVOKE", target = "Ljava/lang/Integer;parseInt(Ljava/lang/String;)I"))
+    private int readMeta(String s) {
+        return "*".equals(s) ? OreDictionary.WILDCARD_VALUE : Integer.parseInt(s);
+    }
+}
