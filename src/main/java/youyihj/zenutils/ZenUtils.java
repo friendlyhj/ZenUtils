1a34308040e310a9ea197c81d4310120b14eabc0
youyihj <youyi580@qq.com>
2023-06-24 17:54:44 +0800

    v1.14.3

--- src/main/java/youyihj/zenutils/ZenUtils.java
+++ src/main/java/youyihj/zenutils/ZenUtils.java
@@ -41,7 +41,7 @@ import java.lang.reflect.Field;
 public class ZenUtils {
     public static final String MODID = "zenutils";
     public static final String NAME = "ZenUtils";
-    public static final String VERSION = "1.14.2";
+    public static final String VERSION = "1.14.3";
     public static final String DEPENDENCIES = "required-after:crafttweaker;after:contenttweaker;required-after:redstoneflux;after:ftbquests;required-after:mixinbooter";
 
     public static Logger forgeLogger;
