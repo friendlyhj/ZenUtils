8882c70c48303ccc36aa4db3507124dbbc5df36e
youyihj <youyi580@qq.com>
2023-09-01 19:00:11 +0800

    expose IZenCommand for ease dump

--- src/main/java/youyihj/zenutils/api/command/IZenCommand.java
+++ src/main/java/youyihj/zenutils/api/command/IZenCommand.java
@@ -1,12 +1,15 @@
 package youyihj.zenutils.api.command;
 
+import crafttweaker.annotations.ZenRegister;
 import net.minecraft.command.ICommand;
+import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
 /**
  * @author youyihj
  */
-@SuppressWarnings("unused")
+@ZenRegister
+@ZenClass("mods.zenutils.command.IZenCommand")
 public interface IZenCommand extends ICommand {
     @ZenMethod
     default void register() {
