c24483daeb4302b5ed235f036ba64637455555a8
youyihj <youyi580@qq.com>
2024-04-23 12:48:02 +0800

    remove debug code

--- src/main/java/youyihj/zenutils/impl/core/ZenUtilsClassTransformer.java
+++ src/main/java/youyihj/zenutils/impl/core/ZenUtilsClassTransformer.java
@@ -1,15 +1,11 @@
 package youyihj.zenutils.impl.core;
 
 import net.minecraft.launchwrapper.IClassTransformer;
-import org.apache.commons.io.FileUtils;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import youyihj.zenutils.api.util.ReflectionInvoked;
 import youyihj.zenutils.impl.core.asm.ASMParsedExpression;
-
-import java.io.File;
-import java.io.IOException;
 
 /**
  * @author youyihj
@@ -22,13 +18,7 @@ public class ZenUtilsClassTransformer implements IClassTransformer {
             ClassWriter classWriter = new ClassWriter(0);
             ASMParsedExpression asm = new ASMParsedExpression(Opcodes.ASM5, classWriter);
             new ClassReader(basicClass).accept(asm, 0);
-            byte[] byteArray = classWriter.toByteArray();
-            try {
-                FileUtils.writeByteArrayToFile(new File("ParsedExpression.class"), byteArray);
-                return byteArray;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+            return classWriter.toByteArray();
         }
         return basicClass;
     }
