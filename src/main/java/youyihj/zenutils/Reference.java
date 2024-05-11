8200cd25254947bcefc0b7de791a995c84c2a0b6
youyihj <youyi580@qq.com>
2024-05-11 18:07:07 +0800

    fix mixinbooter gradle dep

--- src/main/java/youyihj/zenutils/Reference.java
+++ src/main/java/youyihj/zenutils/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
     public static final String MODID = "zenutils";
     public static final String NAME = "ZenUtils";
     public static final String VERSION = "1.18.3";
-    public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:mixinbooter;before:contenttweaker;after:ftbquests;";
+    public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:mixinbooter@[8.0,);before:contenttweaker;after:ftbquests;";
     public static final String MOD_COT = "contenttweaker";
     public static final boolean IS_CLEANROOM = !System.getProperty("java.version").startsWith("1.8");
 }
