e2ca2ae7468f80ec3ab6e9980ff260b650b83278
friendlyhj <youyi580@qq.com>
2024-12-29 23:00:02 +0800

    v1.21.6

--- src/main/java/youyihj/zenutils/Reference.java
+++ src/main/java/youyihj/zenutils/Reference.java
@@ -6,7 +6,7 @@ package youyihj.zenutils;
 public class Reference {
     public static final String MODID = "zenutils";
     public static final String NAME = "ZenUtils";
-    public static final String VERSION = "1.21.5";
+    public static final String VERSION = "1.21.6";
     public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:mixinbooter@[8.0,);required-after:configanytime;before:contenttweaker;after:ftbquests;";
     public static final String MOD_COT = "contenttweaker";
     public static final boolean IS_CLEANROOM = !System.getProperty("java.version").startsWith("1.8");
