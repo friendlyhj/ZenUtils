9205ee39c2a6ebf508b91755ff3dc34c2df45b6d
friendlyhj <youyi580@qq.com>
2025-04-27 19:56:33 +0800

    throw error when trying to run a client catenation on server side. #88

--- src/main/java/youyihj/zenutils/impl/util/catenation/ClientCatenationBuilder.java
+++ src/main/java/youyihj/zenutils/impl/util/catenation/ClientCatenationBuilder.java
@@ -1,5 +1,7 @@
 package youyihj.zenutils.impl.util.catenation;
 
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
 import youyihj.zenutils.api.util.catenation.Catenation;
 
 /**
@@ -8,6 +10,9 @@ import youyihj.zenutils.api.util.catenation.Catenation;
 public class ClientCatenationBuilder extends AbstractCatenationBuilder {
     @Override
     protected void register(Catenation catenation) {
+        if (FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER) {
+            throw new IllegalStateException("Trying to run a client catenation on server side.");
+        }
         CatenationManager.addClientCatenation(catenation);
     }
 }
