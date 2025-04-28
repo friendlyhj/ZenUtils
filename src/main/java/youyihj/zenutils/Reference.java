f08843b7486b42235d5f164ac8c8ae62c93c299c
friendlyhj <youyi580@qq.com>
2025-04-28 19:25:12 +0800

    get java version by apache commons

--- src/main/java/youyihj/zenutils/Reference.java
+++ src/main/java/youyihj/zenutils/Reference.java
@@ -1,5 +1,7 @@
 package youyihj.zenutils;
 
+import org.apache.commons.lang3.SystemUtils;
+
 /**
  * @author youyihj
  */
@@ -9,5 +11,5 @@ public class Reference {
     public static final String VERSION = "1.21.9";
     public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:mixinbooter@[8.0,);required-after:configanytime;before:contenttweaker;after:ftbquests;";
     public static final String MOD_COT = "contenttweaker";
-    public static final boolean IS_CLEANROOM = !System.getProperty("java.version").startsWith("1.8");
+    public static final boolean IS_CLEANROOM = !SystemUtils.IS_JAVA_1_8;
 }
