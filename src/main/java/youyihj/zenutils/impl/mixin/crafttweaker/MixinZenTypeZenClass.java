7eaef8b7a44b527b5d4c2b4425f5f4b7782c71a3
friendlyhj <youyi580@qq.com>
2025-07-11 14:18:44 +0800

    fix wrong impl of ZenTypeZenClass#isPointer

---
+++ src/main/java/youyihj/zenutils/impl/mixin/crafttweaker/MixinZenTypeZenClass.java
@@ -0,0 +1,20 @@
+package youyihj.zenutils.impl.mixin.crafttweaker;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import stanhebben.zenscript.type.ZenTypeZenClass;
+
+/**
+ * @author youyihj
+ */
+@Mixin(value = ZenTypeZenClass.class, remap = false)
+public abstract class MixinZenTypeZenClass {
+    /**
+     * @author youyihj
+     * @reason wrong impl
+     */
+    @Overwrite
+    public boolean isPointer() {
+        return true;
+    }
+}
