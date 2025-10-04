cdd2dde6b3cb931c0a7bcfc5e45b301fa9d85f50
friendlyhj <youyi580@qq.com>
2025-10-04 11:58:39 +0800

    forgot to register this type

--- src/main/java/youyihj/zenutils/api/cotx/function/IBoundingBoxFunction.java
+++ src/main/java/youyihj/zenutils/api/cotx/function/IBoundingBoxFunction.java
@@ -4,11 +4,17 @@ import com.teamacronymcoders.contenttweaker.api.ctobjects.aabb.MCAxisAlignedBB;
 import com.teamacronymcoders.contenttweaker.api.ctobjects.blockpos.IBlockPos;
 import com.teamacronymcoders.contenttweaker.api.ctobjects.blockstate.ICTBlockState;
 import crafttweaker.api.world.IBlockAccess;
+import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
+import youyihj.zenutils.Reference;
+import youyihj.zenutils.api.zenscript.SidedZenRegister;
 
 /**
  * @author youyihj
  */
+@FunctionalInterface
+@SidedZenRegister(modDeps = Reference.MOD_COT)
+@ZenClass("mods.zenutils.cotx.IBoundingBoxFunction")
 public interface IBoundingBoxFunction {
     @ZenMethod
     MCAxisAlignedBB apply(MCAxisAlignedBB origin, ICTBlockState blockState, IBlockAccess world, IBlockPos pos);
