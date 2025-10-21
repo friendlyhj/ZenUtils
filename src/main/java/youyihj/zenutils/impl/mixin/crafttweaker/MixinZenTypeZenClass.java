8f57d33bc6238dc42df807043ec343eb19d935a7
friendlyhj <youyi580@qq.com>
2025-10-21 11:38:17 +0800

    allow .class in custom zenClasses

--- src/main/java/youyihj/zenutils/impl/mixin/crafttweaker/MixinZenTypeZenClass.java
+++ src/main/java/youyihj/zenutils/impl/mixin/crafttweaker/MixinZenTypeZenClass.java
@@ -1,14 +1,26 @@
 package youyihj.zenutils.impl.mixin.crafttweaker;
 
+import org.objectweb.asm.Type;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import stanhebben.zenscript.compiler.IEnvironmentGlobal;
+import stanhebben.zenscript.expression.partial.IPartialExpression;
 import stanhebben.zenscript.type.ZenTypeZenClass;
+import stanhebben.zenscript.util.ZenPosition;
+import youyihj.zenutils.impl.zenscript.nat.ExpressionClassConstant;
 
 /**
  * @author youyihj
  */
 @Mixin(value = ZenTypeZenClass.class, remap = false)
 public abstract class MixinZenTypeZenClass {
+    @Shadow
+    public abstract Type toASMType();
+
     /**
      * @author youyihj
      * @reason wrong impl
@@ -16,5 +28,12 @@ public abstract class MixinZenTypeZenClass {
     @Overwrite
     public boolean isPointer() {
         return true;
+    }
+
+    @Inject(method = "getStaticMember", at = @At("HEAD"), cancellable = true)
+    private void injectClassStaticMember(ZenPosition position, IEnvironmentGlobal environment, String name, CallbackInfoReturnable<IPartialExpression> cir) {
+        if (name.equals("class")) {
+            cir.setReturnValue(new ExpressionClassConstant(position, toASMType()));
+        }
     }
 }
