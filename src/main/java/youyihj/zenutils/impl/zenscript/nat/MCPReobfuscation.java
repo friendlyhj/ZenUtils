a377da48cba8e3df5fc9788ef6ce2c60d26e6b24
friendlyhj <youyi580@qq.com>
2026-02-11 14:12:34 +0800

    bundle compressed mapping into the mod jar

--- src/main/java/youyihj/zenutils/impl/zenscript/nat/MCPReobfuscation.java
+++ src/main/java/youyihj/zenutils/impl/zenscript/nat/MCPReobfuscation.java
@@ -11,20 +11,18 @@ import youyihj.zenutils.impl.member.ExecutableData;
 import youyihj.zenutils.impl.member.FieldData;
 import youyihj.zenutils.impl.member.LookupRequester;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.*;
-import java.security.MessageDigest;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
+import java.util.zip.GZIPInputStream;
 
 /**
  * @author youyihj
@@ -32,10 +30,9 @@ import java.util.stream.Stream;
 public enum MCPReobfuscation {
     INSTANCE;
 
-    private static final int MAX_RETRY = 10;
     private static final Logger LOGGER = LogManager.getLogger();
 
-    private final CompletableFuture<Pair<Multimap<String, String>, Multimap<String, String>>> mappers = CompletableFuture.supplyAsync(this::init);
+    private final CompletableFuture<Pair<Multimap<String, String>, Multimap<String, String>>> mappers = CompletableFuture.supplyAsync(this::unzip);
 
     public FieldData reobfField(ClassData owner, String name, LookupRequester requester) {
         Collection<String> possibleNames = mappers.join().getRight().get(name);
@@ -71,95 +68,51 @@ public enum MCPReobfuscation {
         throw new NoSuchFieldException(name);
     }
 
-    private Pair<Multimap<String, String>, Multimap<String, String>> init() {
+    private Pair<Multimap<String, String>, Multimap<String, String>> unzip() {
+        long startTime = System.currentTimeMillis();
+
         Multimap<String, String> methodMap = HashMultimap.create();
         Multimap<String, String> fieldMap = HashMultimap.create();
-        Path localMapping = Paths.get("config", "mcp_stable-39-1.12.zip");
-        String remoteMapping = "https://maven.minecraftforge.net/de/oceanlabs/mcp/mcp_stable/39-1.12/mcp_stable-39-1.12.zip";
-        if (!Files.exists(localMapping)) {
-            try {
-                downloadWithCheck(remoteMapping, remoteMapping + ".sha1", localMapping);
-            } catch (Exception e) {
-                throw new RuntimeException("Failed to download mcp mapping, try download it manually to `config/mcp_stable-39-1.12.zip`, uri: " + remoteMapping, e);
+
+        try (DataInputStream dis = new DataInputStream(new BufferedInputStream(new GZIPInputStream(Objects.requireNonNull(this.getClass().getResourceAsStream("/mapping")))))) {
+            int prevSrg = dis.readInt();
+            addEntry(methodMap, dis, prevSrg, true);
+            short offset;
+            while ((offset = dis.readShort()) != -1) {
+                prevSrg += offset;
+                addEntry(methodMap, dis, prevSrg, true);
             }
-        }
-        try (FileSystem zipFs = FileSystems.newFileSystem(URI.create("jar:" + localMapping.toUri().toASCIIString()), Collections.emptyMap())) {
-            try (Stream<String> methodStream = Files.lines(zipFs.getPath("methods.csv"))) {
-                methodStream.skip(1)
-                            .map(it -> it.split(","))
-                            .forEach(it -> methodMap.put(it[1], it[0]));
+
+            prevSrg = dis.readInt();
+            addEntry(fieldMap, dis, prevSrg, false);
+            while ((offset = dis.readShort()) != -1) {
+                prevSrg += offset;
+                addEntry(fieldMap, dis, prevSrg, false);
             }
-            try (Stream<String> methodStream = Files.lines(zipFs.getPath("fields.csv"))) {
-                methodStream.skip(1)
-                            .map(it -> it.split(","))
-                            .forEach(it -> fieldMap.put(it[1], it[0]));
-            }
+        } catch (EOFException ignored) {
+
         } catch (IOException e) {
             throw new RuntimeException("Failed to read mcp mapping", e);
         }
+
+        long endTime = System.currentTimeMillis();
+        LOGGER.info("MCP mapping unzip took {} ms", (endTime - startTime));
         return Pair.of(methodMap, fieldMap);
     }
 
-    private static void downloadWithCheck(String fileUrl, String sha1Url, Path dest) throws Exception {
-        String expectedSha1 = downloadSha1(sha1Url);
-
-        for (int i = 1; i <= MAX_RETRY; i++) {
-            LOGGER.info("{} attempts to download file", i);
-            try {
-                downloadFile(fileUrl, dest);
-            } catch (IOException e) {
-                LOGGER.error("Download failed: {}", e.getMessage());
-                continue;
-            }
-            String actualSha1 = calcSha1(dest);
-            if (expectedSha1.equalsIgnoreCase(actualSha1)) {
-                return;
-            } else {
-                LOGGER.error("SHA-1 Verify Error: expected: {}, actual: {}", expectedSha1, actualSha1);
-                Files.deleteIfExists(dest);
-            }
+    private void addEntry(Multimap<String, String> map, DataInputStream dis, int srgId, boolean isMethod) throws IOException {
+        String notchName;
+        int ch1 = dis.read();
+        int ch2 = dis.read();
+        if (ch1 == 0) {
+            notchName = String.valueOf((char) ch2);
+        } else {
+            notchName = "" + (char) ch1 + (char) ch2;
         }
-        throw new IOException("Exceeded maximum retry attempts to download file: " + fileUrl);
+        int deobfNameLen = dis.read();
+        byte[] deobfNameBytes = new byte[deobfNameLen];
+        dis.readFully(deobfNameBytes);
+        String deobfName = StandardCharsets.US_ASCII.newDecoder().decode(ByteBuffer.wrap(deobfNameBytes)).toString();
+        map.put(deobfName, (isMethod ? "func_" : "field_") + srgId + "_" + notchName);
     }
-
-    private static String downloadSha1(String sha1Url) throws IOException {
-        URLConnection connection = new URL(sha1Url).openConnection();
-        connection.setConnectTimeout(5000);
-        connection.setReadTimeout(5000);
-        try (InputStream in = connection.getInputStream();
-             BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-            String line = reader.readLine();
-            if (line == null || line.isEmpty()) {
-                throw new IOException("Empty sha1 file: " + sha1Url);
-            }
-            return line.split("\\s+")[0].trim();
-        }
-    }
-
-    private static void downloadFile(String fileUrl, Path dest) throws IOException {
-        URLConnection connection = new URL(fileUrl).openConnection();
-        connection.setConnectTimeout(5000);
-        connection.setReadTimeout(5000);
-        try (InputStream in = connection.getInputStream()) {
-            Files.copy(in, dest, StandardCopyOption.REPLACE_EXISTING);
-        }
-    }
-
-    private static String calcSha1(Path file) throws Exception {
-        MessageDigest digest = MessageDigest.getInstance("SHA-1");
-        try (InputStream fis = Files.newInputStream(file)) {
-            byte[] buffer = new byte[8192];
-            int len;
-            while ((len = fis.read(buffer)) != -1) {
-                digest.update(buffer, 0, len);
-            }
-        }
-        byte[] hash = digest.digest();
-        StringBuilder sb = new StringBuilder();
-        for (byte b : hash) {
-            sb.append(String.format("%02x", b));
-        }
-        return sb.toString();
-    }
-
 }
