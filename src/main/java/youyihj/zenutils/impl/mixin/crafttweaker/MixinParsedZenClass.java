99dd0e96a203693d1556118013d021f5e11fa041
friendlyhj <youyi580@qq.com>
2026-07-22 10:58:36 +0800

    mixinzs can modify vanilla classes now

--- src/main/java/youyihj/zenutils/impl/mixin/crafttweaker/MixinParsedZenClass.java
+++ src/main/java/youyihj/zenutils/impl/mixin/crafttweaker/MixinParsedZenClass.java
@@ -28,7 +28,6 @@ import stanhebben.zenscript.type.ZenType;
 import stanhebben.zenscript.type.natives.ZenNativeMember;
 import stanhebben.zenscript.util.ZenPosition;
 import youyihj.zenutils.impl.member.ClassData;
-import youyihj.zenutils.impl.member.reflect.ReflectionClassDataFetcher;
 import youyihj.zenutils.impl.mixin.itf.IEnvironmentClassExtension;
 import youyihj.zenutils.impl.mixin.itf.IParsedClassConstructorExtension;
 import youyihj.zenutils.impl.mixin.itf.IParsedZenClassExtension;
@@ -137,10 +136,10 @@ public abstract class MixinParsedZenClass implements IParsedZenClassExtension {
                     ClassData classData;
                     try {
                         classData = InternalUtils.getClassDataFetcher().forName(target);
-                        if (classData.fetcher() instanceof ReflectionClassDataFetcher) {
+                        if (InternalUtils.isClassLoadedOnLCL(classData.name())) {
                             if (InternalUtils.getScriptStatus() == ScriptStatus.INIT) {
                                 isMixinClass = false;
-                                classEnvironment.warning(position, "Skip loading mixin class " + name + ", because the target " + target + " is a non-mod class or already loaded");
+                                classEnvironment.warning(position, "Skip loading mixin class " + name + ", because the target " + target + " is already loaded");
                             }
                         } else if (!NativeClassValidate.isValid(classData, true)) {
                             isMixinClass = false;
